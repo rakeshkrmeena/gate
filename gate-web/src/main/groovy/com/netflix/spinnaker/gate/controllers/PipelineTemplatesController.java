30771e8925df4b14f0245bef01f14c490510f08a
root <root@ai-guardian-remediation-5fd7447d58-j4w7w>
2025-12-11 09:22:10 +0000

    fix: tainted-sql-string-138

--- gate-web/src/main/groovy/com/netflix/spinnaker/gate/controllers/PipelineTemplatesController.java
+++ gate-web/src/main/groovy/com/netflix/spinnaker/gate/controllers/PipelineTemplatesController.java
@@ -87,7 +87,7 @@ public class PipelineTemplatesController {
 
     Map<String, Object> operation = new HashMap<>();
     operation.put(
-        "description", String.format("Create pipeline template '%s'", sanitizeTemplateName(getNameFromTemplate(template))));
+        "description", "Create pipeline template '" + sanitizeTemplateName(getNameFromTemplate(template)) + "'");
     operation.put("application", getApplicationFromTemplate(template));
     operation.put("job", jobs);
 
@@ -135,7 +135,7 @@ public class PipelineTemplatesController {
 
     Map<String, Object> operation = new HashMap<>();
     operation.put(
-        "description", String.format("Update pipeline template '%s'", sanitizeTemplateName(getNameFromTemplate(template))));
+        "description", "Update pipeline template '" + sanitizeTemplateName(getNameFromTemplate(template)) + "'");
     operation.put("application", getApplicationFromTemplate(template));
     operation.put("job", jobs);
 
@@ -156,7 +156,7 @@ public class PipelineTemplatesController {
     jobs.add(job);
 
     Map<String, Object> operation = new HashMap<>();
-    operation.put("description", String.format("Delete pipeline template '%s'", sanitizeTemplateName(id)));
+    operation.put("description", "Delete pipeline template '" + sanitizeTemplateName(id) + "'");
     operation.put("application", application != null ? application : DEFAULT_APPLICATION);
     operation.put("job", jobs);
 
